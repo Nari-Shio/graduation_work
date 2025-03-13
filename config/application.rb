07bc8cbc38d24305cd8f6e1e324d22826fe9f5ed
Nari-Shio <nsmsrk38nu@gmail.com>
2025-03-14 00:21:40 +0900

    add: rubocop and tailwindcss

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,10 @@
-require_relative "boot"
+require 'logger'
 
-require "rails/all"
+# frozen_string_literal: true
+
+require_relative 'boot'
+
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
